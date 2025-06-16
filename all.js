9e3398b4464545bdbee4c96d996fa15a154a2258
RayXoXyyy <huangchengrui552@gmail.com>
2025-06-16 21:46:23 +0800

    happy

--- all.js
+++ all.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
      $('img').hide()
+      $('happy').hide()
+     
+     
      
      $('p').click(function(event){
-     $('p').toggleClass('close');
-     });
      
-     $('p').click(function(event){
+          $('p').toggleClass('close');
+     
      $('img').toggle()
+     
+     $('happy').toggle()
      });
 
 });
